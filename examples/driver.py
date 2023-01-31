811d88117309febed7e17165c4dbfb6f5df6ca69
mcdebarros <mauricio.debarros@ucalgary.ca>
2023-01-31 10:37:33 -0700

    changed shape_matrix() and added gradient_matrix()

--- examples/driver.py
+++ examples/driver.py
@@ -2,23 +2,23 @@ import numpy as np
 
 from fem_1d_heat.element import (
         global_to_local,
-        shape,
+        shape_matrix,
+        gradient_matrix
         )
 
 
 def main():
     print('successfully imported fem_1d_heat')
 
-    z = 3.
-    z_e = np.array([0, 6])
-    print(f'testing global_to_local({z}, {z_e}): {global_to_local(z, z_e)}')
-
-    z = 2.
+    z = 3
     z_e = np.array([1, 4])
     print(f'testing global_to_local({z}, {z_e}): {global_to_local(z, z_e)}')
 
     s = 0.9
-    print(f'testing shape({s}): {shape(s)}')
+    print(f'testing shape_matrix({s}): {shape_matrix(s)}')
+
+    dz = 3
+    print(f'testing gradient_matrix({s}): {gradient_matrix(s,dz)}')
 
 
 if __name__ == '__main__':
